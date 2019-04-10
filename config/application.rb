310c41f22244d0b3467de4bc0db258cf2a214e2b
YOUR NAME <YOUR@EMAIL.com>
2019-04-10 12:30:53 +0700

    chapter 4 Rails-flavored Ruby

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module App
   class Application < Rails::Application
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en 
     config.load_defaults 5.2
   end
 end
