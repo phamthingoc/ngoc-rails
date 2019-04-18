41058dba75d27e7e0bac92a34f0785c63d4c7449
YOUR NAME <YOUR@EMAIL.com>
2019-04-18 09:10:12 +0700

    chapter7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = t "page_title"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
+    base_title = t("layouts.application.page_title")
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_035637) do
+ActiveRecord::Schema.define(version: 2019_04_08_063115) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
