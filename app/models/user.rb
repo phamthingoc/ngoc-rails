67dae406da4808143d1ad520a194da43efecc140
YOUR NAME <YOUR@EMAIL.com>
2019-04-24 11:15:41 +0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,26 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :name,  presence: true,
-    length: {maximum: Settings.user_valid.max_length_name}
+                    length: {maximum: Settings.user_valid.max_length_name}
   validates :email, presence: true,
-    length: {maximum: Settings.user_valid.max_length_email},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
+                    length: {maximum: Settings.user_valid.max_length_email},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
   validates :password, presence: true,
     length: {minimum: Settings.user_valid.min_length_password}
+  before_save :downcase_email
   has_secure_password
-  before_save :email_downcase
 
-  private
-
-  def email_downcase
+  def downcase_email
     email.downcase!
+  end
+
+  def self.digest string
+    if ActiveModel::SecurePassword.min_cost
+      BCrypt::Engine::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
+    BCrypt::Password.create(string, cost: cost)
   end
 end
